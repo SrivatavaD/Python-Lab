63744d1462dc51d5d7654adccb4482644235c59c
SrivastavaD <sdevansh296@gmail.com>
2026-04-22 22:13:36 +0530

    loops examples in python

---
+++ 14.py
@@ -0,0 +1,29 @@
+# This program prints the multiplication table of a number N
+# using a while loop.
+
+# Ask the user to enter a number.
+n = int(input("Enter a number: "))
+
+# Start the counter from 1.
+i = 1
+
+# Run the loop until 10.
+while i <= 10:
+    print(n, "x", i, "=", n * i)
+
+    # Increase the counter by 1 each time.
+    i += 1
+
+print("Numbers from 1 to 100:")
+
+# This program prints numbers from 1 to 100 using a while loop.
+
+# Start counting from 1.
+number = 1
+
+# Keep running the loop until the number becomes greater than 100.
+while number <= 100:
+    print(number)
+
+    # Increase the number by 1 each time.
+    number += 1
