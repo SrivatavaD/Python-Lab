0c9472b14f21d4d682d36d48638f4b5d967010fd
SrivastavaD <sdevansh296@gmail.com>
2026-05-11 23:31:23 +0530

    HackerRank Coding Problem

--- HackerRank Python/1.py
+++ HackerRank Python/1.py
@@ -5,21 +5,21 @@
 # If  is even and in the inclusive range of 6 to 20, print Weird
 # If  is even and greater than , print Not Weird
 
-# import math
-# import os
-# import random
-# import re
-# import sys
-# n = int(input("Enter the number: "))
+import math
+import os
+import random
+import re
+import sys
+n = int(input("Enter the number: "))
 
-# if(n % 2 != 0):
-#     print("Weird")
-# elif(n % 2 == 0 and n >= 2 and n <= 5):
-#     print("Not Weird")
-# elif(n % 2 == 0 and n >= 6 and n <= 20):
-#     print("Weird")
-# else:
-#     print("Not Weird")
+if(n % 2 != 0):
+    print("Weird")
+elif(n % 2 == 0 and n >= 2 and n <= 5):
+    print("Not Weird")
+elif(n % 2 == 0 and n >= 6 and n <= 20):
+    print("Weird")
+else:
+    print("Not Weird")
 
 
 # The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
