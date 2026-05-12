606f11427a20478edf2087852379e91153560260
SrivastavaD <sdevansh296@gmail.com>
2026-05-12 11:01:15 +0530

    HackerRank Python problems

---
+++ HackerRank Python/2.py
@@ -0,0 +1,7 @@
+# The provided code stub reads an integer,n, from STDIN. For all non-negative integers ,i<n  print i^2 .
+# Example
+# The list of non-negative integers that are less than   n = 3 is [0,1,2] . Print the square of each number on a separate line.
+
+n = int(input("Enter the number: "))
+for i in range(n):
+    print(i**2)
