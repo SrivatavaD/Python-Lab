e852cc69c663fff523e1d8dda4dd979d957f9090
SrivastavaD <sdevansh296@gmail.com>
2026-05-13 12:34:04 +0530

    hacker Rank question

--- HackerRank Python/2.py
+++ HackerRank Python/2.py
@@ -2,6 +2,13 @@
 # Example
 # The list of non-negative integers that are less than   n = 3 is [0,1,2] . Print the square of each number on a separate line.
 
-n = int(input("Enter the number: "))
-for i in range(n):
-    print(i**2)
+# n = int(input("Enter the number: "))
+# for i in range(n):
+#     print(i**2)
+
+    # Without using any string methods, try to print the following:
+    #  123...n
+
+n = int(input())
+for n in range(1,n+1): print(n,end="")
+
