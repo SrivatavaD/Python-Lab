0e473c652a64bc9c831225c6d355c13f0c8a930a
SrivastavaD <sdevansh296@gmail.com>
2026-05-14 15:54:26 +0530

    Adding misc concepts in python

--- Misc/1.py
+++ Misc/1.py
@@ -60,10 +60,10 @@
 #     print("Done with the loop") 
 # 
 
-for i in range(12):
-    if(i==10):
-        break
-    print("5 X", i+1, "=", 5*(i+1))
+# for i in range(12):
+#     if(i==10):
+#         break
+#     print("5 X", i+1, "=", 5*(i+1))
 
-print("Done with the loop")
+# print("Done with the loop")
 
