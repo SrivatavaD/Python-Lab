6b927101a52014f34a573431c9d09d9d88233165
SrivastavaD <sdevansh296@gmail.com>
2026-05-15 21:00:02 +0530

    added Mics Concepts

---
+++ Misc/3.py
@@ -0,0 +1,24 @@
+# l = [3,5,7 , "harry" ,True]
+# print(l)
+# print(type(l))
+
+# print(l[-3]) #negative indexing
+# print(l[len(l)-3])  #positive indexing
+
+# if 10 in l:
+#     print("YES")
+# else:
+#     print("No")  
+
+# list = [11,45,1,2]
+# # print(list)
+# list.sort(reverse=True)
+# print(list)
+
+tup = (1,2)
+# tup[0] = 3  # This will raise an error because tuples are immutable
+print(type(tup),tup)
+
+
+
+
