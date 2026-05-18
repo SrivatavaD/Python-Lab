346bf23ebb55695d0a6ea20c13947214828fb8f8
SrivastavaD <sdevansh296@gmail.com>
2026-05-18 20:34:19 +0530

    added new hacker rank question

--- HackerRank Python/2.py
+++ HackerRank Python/2.py
@@ -17,3 +17,29 @@
 
 n = "devansh"
 print(n[-4:-2])
+
+# In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+# NOTE: String letters are case-sensitive.
+# Input Format
+# The first line of input contains the original string. The next line contains the substring.
+
+def count_substring(string, sub_string):
+
+    rep = 0
+    
+    f = string.find(sub_string)
+    while (f != -1):
+        rep = rep + 1
+        string = string[f+1:]
+        f = string.find(sub_string)
+
+    return rep
+    return
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
+
