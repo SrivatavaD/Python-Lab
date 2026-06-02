8bdf7e8259617a154f5094c40023573fca608699
SrivastavaD <sdevansh296@gmail.com>
2026-06-02 16:32:02 +0530

    OOPs Concept in Python

--- Misc/10.py
+++ Misc/10.py
@@ -1,15 +1,28 @@
+# class employee:
+#     def __init__(self,  name, id):
+#         self.name = name
+#         self.id = id
+#     def showdetails(self):
+#         print(f"the name of the employee: {self.id} is {self.name}")
+
+# class programmer(employee):
+#     def showlanguage(self):
+#         print("the default language is python")
+
+# e1 = employee("Devansh", 400)
+# e1.showdetails()
+# e2 = employee("Harry" , 4100)
+# e2.showdetails()        
+# 
+# Access Modifiers in python
+# 
 class employee:
-    def __init__(self,  name, id):
-        self.name = name
-        self.id = id
-    def showdetails(self):
-        print(f"the name of the employee: {self.id} is {self.name}")
+    def __init__(self):
+        self.__name = "devansh"
+        
+a = employee()   #print(a.name) # canniot be accessed directly
 
-class programmer(employee):
-    def showlanguage(self):
-        print("the default language is python")
+print(a._employee__name) # can be accessed indirectly 
 
-e1 = employee("Devansh", 400)
-e1.showdetails()
-e2 = employee("Harry" , 4100)
-e2.showdetails()                    
+print(a.__dir__())
+        
