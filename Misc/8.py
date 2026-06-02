3de22b5269494f1a1146cc9c3a0e215fcf40d524
SrivastavaD <sdevansh296@gmail.com>
2026-06-02 13:40:12 +0530

    OOPs

--- Misc/8.py
+++ Misc/8.py
@@ -1,13 +1,30 @@
-class person:
-    name = "devansh"
-    occupation = "Software Engineer"
-    networth = 10
-    def info(self):
-        print(f"{self.name} is a {self.occupation} with a net worth of {self.networth} million dollars.")
+# class person:
+#     name = "devansh"
+#     occupation = "Software Engineer"
+#     networth = 10
+#     def info(self):
+#         print(f"{self.name} is a {self.occupation} with a net worth of {self.networth} million dollars.")
 
-a = person()
-a.name = "harry"
-a.occupation = "Data Scientist"
-a.info()
+# a = person()
+# a.name = "harry"
+# a.occupation = "Data Scientist"
+# a.info()
 
 # print(a.name)    
+
+class person:
+
+    def __init__(self):
+        print("hey, i am a person")
+
+    # name = "devansh"
+    # occupation = "Software Engineer"
+
+    def info():
+        print(f"{self.name} is a {self.occupation}")
+
+a = person()
+a.name = "Divya"
+a.occ = "HR"
+a.info()
+
