d31d2279fa4e4c3086e450d8e1ef19a55260445a
SrivastavaD <sdevansh296@gmail.com>
2026-06-19 16:45:43 +0530

    Decorators in Python

--- Misc/13.py
+++ Misc/13.py
@@ -10,15 +10,36 @@
 # say_hello()
 
 # login check code
-def login_required(func):
-    def wrapper(user):
-        if user == "admin":
-            func(user)
-        else:
-            print("Access Denied.")
+# def login_required(func):
+#     def wrapper(user):
+#         if user == "admin":
+#             func(user)
+#         else:
+#             print("Access Denied.")
+#     return wrapper
+# @login_required
+# def dashboard(user):
+#     print(f"welcome to the dashboard, {user}")
+# dashboard("admin")
+# dashboard("guest")
+
+def repeat(func):
+    def wrapper():
+        func()
+        func()
     return wrapper
-@login_required
-def dashboard(user):
-    print(f"welcome to the dashboard, {user}")
-dashboard("admin")
-dashboard("guest")
+@repeat
+def say_hi():
+    print("Hi")
+say_hi()
+
+
+def uppercase(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+    return wrapper
+@uppercase
+def message():
+    return "hello python"
+print(message())
