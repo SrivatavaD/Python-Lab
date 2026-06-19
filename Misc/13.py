2833e093d8ac85b8d86f677fe7d5dbd3c7ffdcda
SrivastavaD <sdevansh296@gmail.com>
2026-06-19 14:08:14 +0530

    Decorators in python

--- Misc/13.py
+++ Misc/13.py
@@ -1,10 +1,24 @@
-def my_decorator(func):
-    def wrapper():
-        print("Before functions runs")
-        func()
-        print("After function runs")
+# def my_decorator(func):
+#     def wrapper():
+#         print("Before functions runs")
+#         func()
+#         print("After function runs")
+#     return wrapper
+# @my_decorator
+# def say_hello():
+#     print("Hello!")
+# say_hello()
+
+# login check code
+def login_required(func):
+    def wrapper(user):
+        if user == "admin":
+            func(user)
+        else:
+            print("Access Denied.")
     return wrapper
-@my_decorator
-def say_hello():
-    print("Hello!")
-say_hello()
+@login_required
+def dashboard(user):
+    print(f"welcome to the dashboard, {user}")
+dashboard("admin")
+dashboard("guest")
