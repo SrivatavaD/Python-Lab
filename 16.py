57a0ee26a7f66e8a87fddb241595f0aba9b21395
SrivastavaD <sdevansh296@gmail.com>
2026-08-07 00:58:06 +0530

    Adding new misc question

--- 16.py
+++ 16.py
@@ -12,3 +12,22 @@ def calculator(operator,a,b):
     if operator == '/':
         print(a // b)
 
+x = int(input())
+y = int(input())
+
+x = x - 1
+y = y + 1
+
+print("Value of X decremented by 1:", x)
+print("Value of Y incremented by 1:", y)
+
+
+# Write your code here
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+percentage = (a + b + c + d) / 4
+
+print(round(percentage))
